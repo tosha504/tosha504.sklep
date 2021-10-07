5b4464be4f31e8ee6f3c14d4ca1b74ec5fddf24c
tosha504 <tosha.rovniy@gmail.com>
2021-10-07 19:45:03 +0200

    modification

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-jQuery(document).ready(function ($) {
+window.addEventListener("DOMContentLoaded", () => {
+  const catalog = document.querySelectorAll(".catalog__btn");
 
+  window.addEventListener("resize", function() {
+    let width = window.innerWidth;
+
+    if (width < 1025) {
+      document.querySelector(".catalog__min-width").classList.add("active");
+
+      catalog.forEach((el) => {
+        el.classList.add("d-none");
+      });
+    } else {
+      catalog.forEach((el) => {
+        document
+          .querySelector(".catalog__min-width")
+          .classList.remove("active");
+
+        el.classList.remove("d-none");
+      });
+    }
+  });
+
+  //   catalog.forEach((el) => {
+  //     el.addEventListener("click", function(event) {
+  //       if (this.classList.contains("active")) {
+  //         this.classList.remove("active");
+  //       } else {
+  //         event.target.classList.add("active");
+  //       }
+  //     });
+  //   });
 });
